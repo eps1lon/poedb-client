657b574e7c983aaafbb4378c73e2bec3c21ef7a3
eps1lon <silbermann.sebastian@gmail.com>
2017-07-25 20:17:25 +0200

    run all exported sagas automatically
    retired into this feature branch since i think its bad practice. This way we can't controll what we run programmatically or disable something for debug etc.

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,8 +1,9 @@
+import _ from 'lodash';
 import { all, fork } from 'redux-saga/effects';
 
-import { apiToQueryForm, watchChange } from './form';
-import { watchDirtyTable } from './table';
+import * as form from './form';
+import * as table from './table';
 
 export default function* root() {
-  yield all([fork(watchDirtyTable), fork(apiToQueryForm), fork(watchChange)]);
+  yield all(_.flatten([form, table].map(Object.values)).map(fork));
 }
